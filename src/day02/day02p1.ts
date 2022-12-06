3045a7e86cbdbc3e69842b1eb10b41f6ad05e1c7
Alexander Wiebe <alexanderjwiebe@gmail.com>
2022-12-06 14:10:32 -0600

    got day 2 solved 🎄🎅

--- src/day02/day02p1.ts
+++ src/day02/day02p1.ts
@@ -7,7 +7,25 @@ export function day02p1() {
     ['C', 'Z'],
   ];
 
+  const score: Record<string, number> = { X: 1, Y: 2, Z: 3 };
+
+  /**
+   * returns a key value pair of song and artists
+   */
+  const win: Record<string, Record<string, number>> = {
+    X: { A: 3, B: 0, C: 6 }, // inc by 1
+    Y: { A: 6, B: 3, C: 0 }, // inc by 2
+    Z: { A: 0, B: 6, C: 3 }, // inc by 3
+  };
+
+  return input.reduce((sum, [opponent, elf]) => sum + win[elf][opponent] + score[elf], 0);
+
   /* part 1 
+  1 for Rock, 2 for Paper, and 3 for Scissors
+  0 if you lost, 3 if the round was a draw, and 6 if you won
+  
+  total score of 15 (8 + 1 + 6)
+
   This strategy guide predicts and recommends the following:
 
     In the first round, your opponent will choose Rock (A), and you should choose Paper (Y). This ends in a win for you with a score of 8 (2 because you chose Paper + 6 because you won).
@@ -15,5 +33,4 @@ export function day02p1() {
     The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = 6.
 
   */
-  return Math.max(...input.map((nums) => nums.reduce((sum, val) => sum + val)));
 }
