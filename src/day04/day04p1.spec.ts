cf5da0b361d2f52aa748bf44e4d01d4d5f1ff093
Alexander Wiebe <alexanderjwiebe@gmail.com>
2022-12-09 11:36:54 -0600

    adding day 04 for todays stream

---
+++ src/day04/day04p1.spec.ts
@@ -0,0 +1,5 @@
+describe('day 04', () => {
+  it('should true be true', () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ src/day04/day04p2.ts
@@ -0,0 +1,7 @@
+export function day04p2() {
+  /* Part 1 */
+
+  /* Part 2 */
+
+  let example = [];
+}
