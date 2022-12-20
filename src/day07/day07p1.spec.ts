47c2f9b05fce76beaf89bb9a7451e7334f9c1ce9
Alexander Wiebe <alexanderjwiebe@gmail.com>
2022-12-20 12:25:14 -0600

    getting ready for day 7

---
+++ src/day07/day07p1.spec.ts
@@ -0,0 +1,5 @@
+describe('day 07', () => {
+  it('should true be true', () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ src/day07/day07p2.spec.ts
@@ -0,0 +1,5 @@
+describe('day 07p2', () => {
+  it('should true be true', () => {
+    expect(true).toBeTruthy();
+  });
+});
