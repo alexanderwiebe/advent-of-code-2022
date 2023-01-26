c1c6c00400d8349398689899bab4d27caa6d270d
Wade H <wade@bitovi.com>
2023-01-26 13:41:52 -0500

    Day 1 Setup

---
+++ src/2015-day01/day01p1.ts
@@ -0,0 +1,10 @@
+/*
+import * as fs from 'node:fs';
+fs.readFileSync(filename)
+    .toString()
+    .split('\n')
+*/
+
+export function day01p1() {
+    
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 // just a runner of the days specific code
 
-import { input } from './day07/data';
-import { day07p1 } from './day07/day07p1';
+import { day01p1 } from './2015-day01/day01p1';
 
-console.log(day07p1(input.slice(1, input.length)));
+console.log(day01p1());
